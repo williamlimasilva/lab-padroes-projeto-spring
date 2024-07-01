cc9d7e258dee65e8eb1c1325289b94c9b92cc65c
williamlimasilva <williamtakuma@yahoo.com.br>
2024-07-01 12:08:38 -0300

    Fist Commit

---
+++ src/main/java/one/digitalinnovation/gof/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.gof.repository;
+
+import one.digitalinnovation.gof.model.Cliente;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente,Long> {
+}
